8d2c124a226a6b43fb222af3684140244e4caf9e
Nils Martel <nilsmartel@yahoo.de>
2022-10-10 11:52:02 +0200

    Write code to connect using sqlx

---
+++ src/db.rs
@@ -0,0 +1,22 @@
+/// returns (User, Password, Database)
+fn get_credentials() -> Result<[String; 3]> {
+    use std::env::var;
+
+    Ok([
+        var("DATABASE_USER")?,
+        var("DATABASE_PASSWORD")?,
+        var("DATABASE_DB")?,
+    ])
+}
+
+// example string:
+//  "postgresql://dboperator:operatorpass123@localhost:5243/postgres"
+fn get_config_str([user, password, database]: [String; 3]) -> String {
+    format!("postgresql://{user}:{password}@localhost/{database}")
+}
+
+pub fn client_str() -> String {
+    get_credentials()
+        .map(get_config_str)
+        .expect("to read credentials for database")
+}
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,41 @@
+mod db;
+
 use sqlx::postgres::PgPoolOptions;
 // use sqlx::mysql::MySqlPoolOptions;
 // etc.
 
-#[async_std::main]
+#[tokio::main]
 // or #[tokio::main]
 // or #[actix_web::main]
 async fn main() -> Result<(), sqlx::Error> {
+    let args = std::env::args().skip(1).collect::<Vec<String>>();
+    if args.len() != 2 {
+        println!("<corpus> <limit>");
+        std::process::exit(0);
+    }
+
+    let corpus = args[0].clone();
+    let limit = args[0]
+        .parse::<u64>()
+        .expect("second argument to be positive integer");
+
     // Create a connection pool
     //  for MySQL, use MySqlPoolOptions::new()
     //  for SQLite, use SqlitePoolOptions::new()
     //  etc.
     let pool = PgPoolOptions::new()
         .max_connections(5)
-        .connect("postgres://postgres:password@localhost/test").await?;
+        .connect(&db::client_str())
+        .await?;
 
-    // Make a simple query to return the given parameter (use a question mark `?` instead of `$1` for MySQL)
-    let row: (i64,) = sqlx::query_as("SELECT $1")
-        .bind(150_i64)
-        .fetch_one(&pool).await?;
+    let row: (i64,) = sqlx::query_as("SELECT tokenized from $1 limit $2")
+        .bind(corpus)
+        .bind(limit)
+        .fetch_one(&pool)
+        .await?;
 
-    assert_eq!(row.0, 150);
+    dbg!(&row);
 
     Ok(())
 }
+
