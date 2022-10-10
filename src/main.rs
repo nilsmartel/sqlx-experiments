ce6040056a48c68d1d5dd6c8e0178562fdc5a122
Nils Martel <nilsmartel@yahoo.de>
2022-10-10 18:55:56 +0200

    Include code to create binary with new adapters

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 mod db;
-use std::io::prelude::*;
 
 use sqlx::postgres::PgPoolOptions;
 use tokio_stream::StreamExt;
@@ -7,52 +6,69 @@ use tokio_stream::StreamExt;
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
-enum Config {
-    Query {
-        #[structopt()]
-        query: String,
-    },
-    Corpus {
-        #[structopt()]
-        corpus: String,
-        #[structopt()]
-        limit: u64,
-    },
+struct Config {
+    #[structopt()]
+    corpus: String,
+
+    #[structopt()]
+    outfile: String,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
     let config = Config::from_args();
+    let corpus = &config.corpus;
+
+    if config.outfile.is_empty() {
+        panic!("outfile mustnt be empty");
+    }
 
     let pool = PgPoolOptions::new()
         .max_connections(5)
         .connect(&db::client_str())
         .await?;
 
-    let query = match config {
-        Config::Corpus { corpus, limit } => format!(
-            "SELECT tokenized
-            FROM {corpus}
-            ORDER BY tokenized
-            LIMIT {limit}"
-        ),
-        Config::Query { query } => query,
-    };
+    let limit = 1_000_000;
+    let mut offset = 0;
 
-    let query = sqlx::query_as::<_, (String,)>(&query);
+    let mut output = std::fs::File::create(&config.outfile).expect("to create outfile");
 
-    let mut stream = query.fetch(&pool);
+    loop {
+        eprintln!("reading rows after size {offset}");
 
-    let stdout = std::io::stdout();
-    let mut out = stdout.lock();
+        let query = format!(
+            "SELECT tokenized tableid colid rowid
+                FROM {corpus}
+                ORDER BY tokenized
+                OFFSET {offset}
+                LIMIT {limit}"
+        );
 
-    let mut last = String::from("zzzzzzzhjksdhfkdj");
-    while let Some(row) = stream.try_next().await? {
-        let s = row.0;
-        if s != last {
-            writeln!(&mut out, "'{s}'").unwrap();
-            last = s;
+        let query = sqlx::query_as::<_, (String, i32, i32, i64)>(&query);
+
+        let mut stream = query.fetch(&pool);
+
+        let mut count = 0;
+        while let Some(row) = stream.try_next().await? {
+            let (tokenized, tableid, colid, rowid) = row;
+
+            let row = bintable::TableRow {
+                tokenized,
+                tableid: tableid as u32,
+                colid: colid as u32,
+                rowid: rowid as u64,
+            };
+
+            row.write_bin(&mut output).expect("write to outfile");
+
+            count += 1;
         }
+
+        if count == 0 {
+            break;
+        }
+
+        offset += limit;
     }
 
     Ok(())
